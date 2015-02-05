96c0cd0f69fd6d337182d9d50e0725297b0dff4f
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2015-02-05 01:51:54 -0500

    added material angular

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -1,30 +1,28 @@
 'use strict';
 require('angular-ui-router');
-
-
-
+require('ngMaterial');
 
 var modulename = 'home';
 
 module.exports = function(namespace) {
 
-    var fullname = namespace + '.' + modulename;
+  var fullname = namespace + '.' + modulename;
 
-    var angular = require('angular');
-    var app = angular.module(fullname, ['ui.router', ]);
-    // inject:folders start
-    require('./controllers')(app);
-    // inject:folders end
+  var angular = require('angular');
+  var app = angular.module(fullname, ['ui.router', 'ngMaterial']);
+  // inject:folders start
+  require('./controllers')(app);
+  // inject:folders end
 
-    app.config(['$stateProvider', '$urlRouterProvider',
-        function($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise('/');
-            $stateProvider.state('home', {
-                url: '/',
-                template: require('./views/home.html')
-            });
-        }
-    ]);
+  app.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
+      $stateProvider.state('home', {
+        url: '/',
+        template: require('./views/home.html')
+      });
+    }
+  ]);
 
-    return app;
+  return app;
 };
