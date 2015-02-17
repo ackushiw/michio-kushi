666d6d4ecd4125549a3ba885e2624ea2c7a613a2
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2015-02-17 02:09:28 -0500

    worked on background and added ui-view to create

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -25,19 +25,47 @@ module.exports = function(namespace) {
     function($stateProvider, $urlRouterProvider) {
       $urlRouterProvider.otherwise('/memories');
       $stateProvider.state('home', {
-        url: '/',
-        template: require('./views/home.html'),
-        controller: fullname + '.main',
-        controllerAs: 'homeCtrl'
-      }).state('home.feed', {
-        url: 'memories',
-        template: require('./views/feed.html')
-      }).state('home.create', {
-        url: 'create',
-        template: require('./views/create.html'),
-        controller: fullname + '.post',
-        controllerAs: 'createCtrl'
-      });
+          url: '/',
+          template: require('./views/home.html'),
+          controller: fullname + '.main',
+          controllerAs: 'homeCtrl'
+        }).state('home.feed', {
+          url: 'memories',
+          template: require('./views/feed.html')
+        }).state('home.create', {
+          url: 'create',
+          template: require('./views/create.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        }).state('home.create.story', {
+          url: '/story',
+          template: require('./views/create/story.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        }).state('home.create.image', {
+          url: '/image',
+          template: require('./views/create/image.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        })
+        .state('home.create.video', {
+          url: '/video',
+          template: require('./views/create/video.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        })
+        .state('home.create.article', {
+          url: '/article',
+          template: require('./views/create/article.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        })
+        .state('home.create.recipe', {
+          url: '/recipe',
+          template: require('./views/create/recipe.html'),
+          controller: fullname + '.post',
+          controllerAs: 'createCtrl'
+        });
     }
   ]);
   app.config(['$mdThemingProvider', function($mdThemingProvider) {
