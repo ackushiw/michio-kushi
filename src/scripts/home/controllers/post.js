f7e9b2ed9400327705ad92cfc63128cd5bb38045
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2015-02-16 19:47:38 -0500

    got embedly service working

--- src/scripts/home/controllers/post.js
+++ src/scripts/home/controllers/post.js
@@ -4,32 +4,65 @@ var controllername = 'post';
 module.exports = function(app) {
   /*jshint validthis: true */
 
-  var deps = [app.name + '.feedFactory'];
+  var deps = [app.name + '.feedFactory', '$mdToast', 'embedlyService'];
 
-  function controller(feedFactory) {
+  function controller(feedFactory, $mdToast, embedlyService) {
     var vm = this;
+
     vm.message = 'Hello Post World';
     var activate = function() {
-      vm.lastPost = null;
+
+      //locker.put('test', vm.message);
+
       vm.currentPost = null;
+      vm.recover = editLast;
+      vm.processUrl = urlExtract;
 
     };
     activate();
 
     vm.post = function(data) {
+      //locker.put('lastPost', data);
       var preparedData = data;
       preparedData.firebaseTimeStamp = new Date();
+      //preparedData.$priority = data.title;
       console.log(preparedData);
       vm.sync = feedFactory.data();
-      vm.sync.$push(preparedData).then(function(ref) {
-        vm.currentPost = ref.key();
+      vm.sync.$push(preparedData).then(function(fireData) {
+        vm.currentPost = fireData.key();
         vm.lastPost = feedFactory.asObject(vm.currentPost);
-        console.log('success', ref.key());
+        //locker.put('lastPost', fireData);
+        console.log('success', fireData.key());
       }, function(error) {
         console.log('Error', error);
       });
       vm.entry = null;
+    };
+
+    function urlExtract(url) {
+      console.log(embedlyService);
+      embedlyService.extract(url).then(function(data) {
+        console.log('data', data);
+        vm.articleData = data;
+      }, function(error) {
+        console.log('Error', error);
+        vm.articleData = error;
+
+      });
+
     }
+
+    function editLast() {
+      vm.currentPost = vm.lastPost;
+      if(!vm.currentPost) {
+        $mdToast.show($mdToast.simple().content('Cannot load last post'));
+      }
+    }
+
+    function urlErrorToast() {
+      $mdToast.show($mdToast.simple().content('Cannot load data from URL'));
+    }
+
   }
 
   controller.$inject = deps;
--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -4,6 +4,7 @@ require('ngMaterial');
 require('angularfire');
 require('angular-youtube-mb');
 require('angular-embedly');
+//require('angular-locker');
 
 var modulename = 'home';
 
@@ -43,8 +44,14 @@ module.exports = function(namespace) {
       .accentPalette('orange');
   }]);
   app.config(['embedlyServiceProvider', function(embedlyServiceProvider) {
-    embedlyServiceProvider.setKey('8d5fa3afe41a41d392b52af11adafbac');
-  }])
+      embedlyServiceProvider.setKey('8d5fa3afe41a41d392b52af11adafbac');
+    }])
+    // app.config(['lockerProvider', function config(lockerProvider) {
+    //   lockerProvider.setDefaultDriver('local')
+    //     .setDefaultNamespace('kushi')
+    //     .setSeparator('.')
+    //     .setEventsEnabled(false);
+    // }]);
 
   return app;
 };
